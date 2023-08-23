9ae8179c7f61bf8d3f80db379a62a6a8613f2b7a
Amber <ambercs1405@gmail.com>
2023-08-23 16:34:43 +0530

    Payments Bug fix

--- server/routes/Payments.js
+++ server/routes/Payments.js
@@ -1,7 +1,7 @@
 // Import the required modules
 const express = require("express")
 const router = express.Router()
-const { capturePayment, verifyPayment, sendPaymentSuccessEmail } = require("../controllers/payments")
+const { capturePayment, verifyPayment, sendPaymentSuccessEmail } = require("../controllers/Payments")
 const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth")
 router.post("/capturePayment", auth, isStudent, capturePayment)
 router.post("/verifyPayment", auth, isStudent, verifyPayment)
