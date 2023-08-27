f3943c61fe57e0b294120f7285acc92930c05e28
Amber <ambercs1405@gmail.com>
2023-08-27 10:51:37 +0530

    Bug fix

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -24,7 +24,7 @@ exports.resetPasswordToken = async (req, res) => {
     )
     console.log("DETAILS", updatedDetails)
 
-    const url = `http://localhost:4000/update-password/${token}`
+    const url = `http://localhost:3000/update-password/${token}`
     // const url =  `https://study-notion-pi.vercel.app/update-password/${token}`
 
     await mailSender(
